2ffd9d523d9a28e78f573fbd9d15f3cfb70cfc26
Sanath Waghela <143343230+Sanathw1212@users.noreply.github.com>
2024-05-26 05:06:26 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,5 @@
+function skillsMember() {
+    const member = 'John';
+    const skills = ['HTML', 'CSS', 'JS'];
+    console.log(`${member} has ${skills.length} skills, and they are: ${skills.join(', ')}`);
+}
